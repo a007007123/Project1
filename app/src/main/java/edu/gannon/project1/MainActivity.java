c8b2593ca078e574a319aca72b87b01b2dcd178c
Han Zhang <a007007123@gmail.com>
2015-02-02 13:04:13 -0500

    project1

---
+++ app/src/main/java/edu/gannon/project1/MainActivity.java
@@ -0,0 +1,61 @@
+package edu.gannon.project1;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+
+        final TextView Text = (TextView) findViewById(R.id.textView);
+
+        final Button Button1 = (Button) findViewById(R.id.button);
+
+        final Button Button2 = (Button) findViewById(R.id.button2);
+
+        Button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Text.setTextColor(Color.RED);
+            }
+        } );
+
+        Button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button2.setTextColor(Color.GREEN);
+            }
+        } );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
